f36f13a1c603d45de45ec62ae52ee5392b89675f
megapelsh <tkachenkovladv@gmail.com>
2022-04-13 16:22:44 +0300

    finished unit_08

--- unit_08/src/App.js
+++ unit_08/src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {createElement, createRef, useState} from 'react'
 
 function App() {
+  
+
+  const h1 = createElement('h1', {}, 'unit_08')
+  const h2 = createElement('h2', {className: 'text-orange'}, 'header 2')
+  const p = createElement('p', {style : {color : 'red'}}, 'this is p')
+  const input = createElement('input', {defaultValue : 55})
+  const p1 = createElement('p', {}, 'hi')
+  const p2 = createElement('p', {}, 'world')
+  const div = createElement('div', {className : 'text-grey'}, p1, p2)
+  
+  const val = createRef()
+  const [li, setLi] = useState([])
+
+  const addLi = () => {
+    if (val.current.value) {
+      const newLi = createElement('li', {}, val.current.value)
+      setLi(newLi)
+    }
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {h1}
+      {h2}
+      {p}
+      {div}
+      {input}
+
+      <div>
+        <input type="text" ref={val} />
+        <button onClick={addLi}>Push me</button>
+        <ul>
+          {li}
+        </ul>
+      </div>    
+      
+    </>
   );
 }
 
